52f5f649a8c58471c99d901dac5efaac4fc37fe2
Hactle <kutevivan932@gmail.com>
2026-02-10 01:08:47 +1000

    Health bar
    -Improved Health bar animation

--- Assets/Scripts/HealthView.cs
+++ Assets/Scripts/HealthView.cs
@@ -1,18 +1,60 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(Slider))]
 public class HealthView : MonoBehaviour
 {
-    private Slider _healthSlider;
+    [Header("Health Fill")]
+    [SerializeField] private Image _redHealthFill;
+    [SerializeField] private Image _whiteHealthFill;
+
+    [Header("Health Change")]
+    [SerializeField] private float _whiteDelay;
+    [SerializeField] private float _whiteSpeed;
+
+    private float _targetFill;
+    private float _redCurrent;
+    private float _whiteCurrent;
+    private float _whiteDelayTimer;
 
     private void Awake()
     {
-        _healthSlider = GetComponent<Slider>();
+        _redCurrent = _redHealthFill.fillAmount;
+        _whiteCurrent = _whiteHealthFill.fillAmount;
+        _targetFill = _redCurrent;
     }
 
     public void SetHealth(float healthPercentage)
     {
-        _healthSlider.value = healthPercentage;
+        healthPercentage = Mathf.Clamp01(healthPercentage);
+
+        if(healthPercentage < _targetFill)
+        {
+            _whiteDelayTimer = _whiteDelay;
+            _whiteCurrent = _redCurrent;
+        }
+        
+        _targetFill = healthPercentage;
+    }
+
+    void Update()
+    {
+        _redCurrent = _targetFill;
+
+        _redHealthFill.fillAmount = _redCurrent;
+
+        if (_whiteDelayTimer > 0f)
+        {
+            _whiteDelayTimer -= Time.deltaTime;
+        }
+        else
+        {
+            _whiteCurrent = Mathf.MoveTowards(
+                _whiteCurrent,
+                _redCurrent,
+                _whiteSpeed * Time.deltaTime
+            );
+
+            _whiteHealthFill.fillAmount = _whiteCurrent;
+        }
     }
 }
